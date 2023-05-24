3a179f6a553cbc93c7a75686ef4c1e7406f5e653
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-05-25 00:38:42 +0200

    Adding route, controller, view Test

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,6 @@
+@extends('base')
+
+@section('content')
+    <h1>Test Page</h1>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/test')->name('test.')->controller(TestController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
 });
